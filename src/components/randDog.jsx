a4e7290c092a9091d61e60a3f0df74283f088d7c
Kristi Hwang <kristi.y.l.h@gmail.com>
2024-05-15 17:15:43 -0700

    create question state and fix checkAnswer function

--- src/components/randDog.jsx
+++ src/components/randDog.jsx
@@ -1,17 +1,21 @@
 import React from "react"
 
-
 export default function RandDog(props) {
 
     return(
+
         props.dogImgs.map((randDog, index) => {
             return (
                 <div key={index} className ="rand-dog-tile">
-                    <button type="button" disabled ={!props.gameStat.isGame} onClick={() => props.handleClick(randDog)}
+                    <button disabled ={!props.gameStat.isGame} onClick={() => props.handleClick(randDog)}
                     >
-                        <img src={randDog}></img>
-                        <h3>{props.currentDog.isSelected? props.currentDog.name: ""}</h3>
+                    <img src={randDog}></img>
                     </button>
+                    
+                    <h3 
+                    // className={props.currentDog.isCorrect? "correct-answer": "wrong-answer"} 
+                    >
+                        {props.currentDog.isSelected? props.currentDog.name: ""}</h3>
                 
                 </div>
             )
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -9,6 +9,7 @@ export async function loader () {
 
 export default function Quiz() {
     const dogImgs = useLoaderData()
+    const [dogToMatch, setDogToMatch] = React.useState(question())
    
     const [currentDog, setCurrentDog] = React.useState({
         key: "",
@@ -20,10 +21,11 @@ export default function Quiz() {
     const [gameStat, setGameStat] = React.useState({
         tries: 0,
         answered: 0,
-        isGame: true,
+        isGame: true
     })
 
-    useEffect(()=> {
+    useEffect(
+        function gameOverTracker() {
         const isMaxTries = gameStat.tries >= 3
         const isMaxAnswered = gameStat.answered >= 10
 
@@ -32,32 +34,53 @@ export default function Quiz() {
         } 
     }, [currentDog])
 
+    // useEffect(
+    //     function getQuestionSet() {
+    //         if (questionList === 4) {
+    //             if (currentDog.answered || currentDog.tries){
+    //                 const newSet = getRandomImgs()
+    //                 return newSet
+    //             }
+                
+    //         }
+    //     }, [gameStat])
+
     function question(){
         const rand = Math.floor(Math.random() * 3)
         const chosen = dogImgs[rand]
-        console.log(chosen)
+        console.log(getBreed(chosen))
         return getBreed(chosen)
-    }
-
+        }
+    
     function getBreed(url= "") {
         const name = url.substring(30).split("/", 1).join()
         console.log(name)
         const capName = name[0].toUpperCase()+name.slice(1).toLowerCase()
-        console.log(name)
+        console.log(capName)
         return capName
     }
  
-    function checkAnswer() {
-        if (currentDog.name !== question()) {
+    function checkAnswer(url) {
+        const correctAnswer = currentDog.name === dogToMatch
+        console.log(correctAnswer)
+        console.log(question())
+        console.log(currentDog.name)
+        if (correctAnswer) {
+            setCurrentDog(prev => ({
+                ...prev,
+                isCorrect: true,
+                name: getBreed(url)
+            })) 
+            console.log(currentDog.name)
+            setGameStat(prev => ({
+                ...prev,
+                answered: prev.answered +1
+            }))
+        } else {
             setGameStat(prev => ({
                 ...prev, 
                 tries: prev.tries +1,
             }))
-        } else {
-            setGameStat(prev => ({
-                ...prev,
-                answered: prev.answered +1
-            })) 
         }
     }
     
@@ -78,20 +101,22 @@ export default function Quiz() {
     }
 
     function handleClick(url) {
+        
         setCurrentDog(prev => ({
             ...prev,
             isSelected: true,
-            name: getBreed(url),
+            name: getBreed(url)
         })) 
-        checkAnswer()
-    }
+        checkAnswer(url)
+    } console.log(currentDog.name)
+    
 
     return(
         <div>
             <div className="title-container">
                 {gameStat.isGame? <h1 className="title"> Quiz Time!</h1>: <button onClick={newGame}>Start Over?</button>}
             </div>
-            <h3>Select the {question()}</h3>
+            <h3>Select the {question(dogToMatch)}</h3>
             <div className="rand-dog-list">
                 { <RandDog
                     dogImgs={dogImgs}
